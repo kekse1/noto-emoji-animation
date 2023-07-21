5b233263feb3f751a95221907d20ab7f32759357
Sebastian 'Kuchen' Kucharczyk <kuchen@kekse.biz>
2023-07-21 20:00:42 +0200

    [1689962441184722754] Freitag, 2023-07-21 (20:00:41)

--- php/emoji.sh
+++ php/emoji.sh
@@ -1,1 +1,14 @@
-../sh/emoji.sh
+#!/usr/bin/env bash
+#
+real="$(realpath "$0")"
+dir="$(dirname "$real")"
+php="`which php 2>/dev/null`"
+
+if [[ -z "$php" ]]; then
+	echo " >> No \`php\` interpreter found!" >&2
+	exit 1
+fi
+
+#
+$php $dir/emoji.php "$@"
+
